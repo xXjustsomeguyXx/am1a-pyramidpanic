80b5304fe4211994d01ed86b34d6c2f949979a80
Roy Kuijper <roy97@casema.nl>
2014-01-06 11:00:06 +0100

    Onze eerste scorpion toegevoegd

---
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Scorpion/Scorpion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+   public class Scorpion
+    {
+       //field
+       private Texture2D texture;
+
+       //properties
+
+
+       //Constructor
+       public Scorpion()
+       {
+
+       }
+
+       //update
+
+
+
+
+    }
+}
